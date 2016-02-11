060837f4f4119b2ba22753116806132080db8a81
bernardo5 <bernardo.n.gomes@ist.utl.pt>
2016-02-11 22:23:45 +0000

    site

---
+++ web/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+
+Name       : Skeleton 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20130902
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+<div id="page" class="container">
+	<div id="header">
+		<div id="logo">
+			<img src="images.png" alt="" />
+			<h1><a href="#">Kizomba</a></h1>
+			<span>Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>, adapted by <a href="https://github.com/bernardo5" rel="nofollow">Bernardo</a></span>
+		</div>
+		<div id="menu">
+			<ul>
+				<li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
+				<li><a href="#" accesskey="2" title="">Festas</a></li>
+				<li><a href="#" accesskey="3" title="">Aulas</a></li>
+				<li><a href="#" accesskey="4" title="">Festivais</a></li>
+				<li><a href="contact.html" accesskey="5" title="">Contacto</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="main">
+		<div id="banner">
+			<img src="flashmob.jpg" alt="" class="image-full" />
+		</div>
+		<div id="welcome">
+			<div class="title">
+				<h2>Propósito</h2>
+			</div>
+			<p>Farto de andar à procura no Facebook das festas? Farto de haver tanta gente a colocar festas que já não encontra a festa que tinha pensado ir? Não se preocupe! Agora é possível filtrar as festas por professor, local, entidade organizadora, etc... Divirta-se e boas kizombadas ;)</p>
+			<ul class="actions">
+				<li><a href="#" class="button">Etiam posuere</a></li>
+			</ul>
+		</div>
+		<div id="featured">
+			<div class="title">
+				<h2>Próximas festas</h2>
+			</div>
+			<p>As festas mais próximas são as apresentadas de seguida. Para outro tipo de pesquisas, ver separador <a href="festas.php"><strong>festas.</strong></a></p>
+			</div>
+			<ul class="style1">
+				<li class="first">
+					<p class="date"><a href="#">Jan<b>05</b></a></p>
+					<h3>Amet sed volutpat mauris</h3>
+					<p><a href="#">Consectetuer adipiscing elit. Nam pede erat, porta eu, lobortis eget, tempus et, tellus. Etiam neque. Vivamus consequat lorem at nisl. Nullam non wisi a sem semper eleifend. Etiam non felis. Donec ut ante.</a></p>
+				</li>
+				<li>
+					<p class="date"><a href="#">Jan<b>03</b></a></p>
+					<h3>Sagittis diam dolor amet</h3>
+					<p><a href="#">Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Mauris quam enim, molestie. Donec leo, vivamus fermentum nibh in augue praesent congue rutrum.</a></p>
+				</li>
+				<li>
+					<p class="date"><a href="#">Jan<b>01</b></a></p>
+					<h3>Amet sed volutpat mauris</h3>
+					<p><a href="#">Consectetuer adipiscing elit. Nam pede erat, porta eu, lobortis eget, tempus et, tellus. Etiam neque. Vivamus consequat lorem at nisl. Nullam non wisi a sem semper eleifend. Etiam non felis. Donec ut ante.</a></p>
+				</li>
+				<li>
+					<p class="date"><a href="#">Dec<b>31</b></a></p>
+					<h3>Sagittis diam dolor amet</h3>
+					<p><a href="#">Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Mauris quam enim, molestie. Donec leo, vivamus fermentum nibh in augue praesent congue rutrum.</a></p>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+</body>
+</html>
